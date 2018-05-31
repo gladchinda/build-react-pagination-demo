b681e8de0431d9a00577bc8e7657d9dd43077bcb
Glad Chinda <gladxeqs@gmail.com>
2018-05-31 11:45:43 +0100

    Made few changes and added comments to components

--- src/App.js
+++ src/App.js
@@ -35,22 +35,29 @@ class App extends Component {
     return (
       <div className="container mb-5">
         <div className="row d-flex flex-row py-5">
-					<div className="w-100 px-4 py-5 d-flex flex-row flex-wrap align-items-center justify-content-between">
+
+          <div className="w-100 px-4 py-5 d-flex flex-row flex-wrap align-items-center justify-content-between">
             <div className="d-flex flex-row align-items-center">
+
               <h2 className={headerClass}>
                 <strong className="text-secondary">{totalCountries}</strong> Countries
               </h2>
+
               { currentPage && (
                 <span className="current-page d-inline-block h-100 pl-4 text-secondary">
                   Page <span className="font-weight-bold">{ currentPage }</span> / <span className="font-weight-bold">{ totalPages }</span>
                 </span>
               ) }
+
             </div>
+
             <div className="d-flex flex-row py-4 align-items-center">
               <Pagination totalRecords={totalCountries} pageLimit={18} pageNeighbours={1} onPageChanged={this.onPageChanged} />
             </div>
           </div>
+
           { currentCountries.map(country => <CountryCard key={country.cca3} country={country} />) }
+
         </div>
       </div>
     );
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,10 +4,14 @@ import PropTypes from 'prop-types';
 const LEFT_PAGE = 'LEFT';
 const RIGHT_PAGE = 'RIGHT';
 
+/**
+ * Helper method for creating a range of numbers
+ * range(1, 5) => [1, 2, 3, 4, 5]
+ */
 const range = (from, to, step = 1) => {
   let i = from;
   const range = [];
-    
+
   while (i <= to) {
     range.push(i);
     i += step;
@@ -25,6 +29,7 @@ class Pagination extends Component {
     this.pageLimit = typeof pageLimit === 'number' ? pageLimit : 30;
     this.totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
 
+    // pageNeighbours can be: 0, 1 or 2
     this.pageNeighbours = typeof pageNeighbours === 'number'
       ? Math.max(0, Math.min(pageNeighbours, 2))
       : 0;
@@ -53,79 +58,95 @@ class Pagination extends Component {
     this.setState({ currentPage }, () => onPageChanged(paginationData));
   }
 
-  handleClick = (page, evt) => {
+  handleClick = page => evt => {
     evt.preventDefault();
     this.gotoPage(page);
   }
 
-  handleMoveLeft = (evt) => {
+  handleMoveLeft = evt => {
     evt.preventDefault();
     this.gotoPage(this.state.currentPage - (this.pageNeighbours * 2) - 1);
   }
 
-  handleMoveRight = (evt) => {
+  handleMoveRight = evt => {
     evt.preventDefault();
     this.gotoPage(this.state.currentPage + (this.pageNeighbours * 2) + 1);
   }
 
+  /**
+   * Let's say we have 10 pages and we set pageNeighbours to 2
+   * Given that the current page is 6
+   * The pagination control will look like the following:
+   *
+   * (1) < {4 5} [6] {7 8} > (10)
+   *
+   * (x) => terminal pages: first and last page(always visible)
+   * [x] => represents current page
+   * {...x} => represents page neighbours
+   */
   fetchPageNumbers = () => {
 
     const totalPages = this.totalPages;
     const currentPage = this.state.currentPage;
     const pageNeighbours = this.pageNeighbours;
-    
+
+    /**
+     * totalNumbers: the total page numbers to show on the control
+     * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
+     */
     const totalNumbers = (this.pageNeighbours * 2) + 3;
     const totalBlocks = totalNumbers + 2;
 
     if (totalPages > totalBlocks) {
 
-      let pages = [];
+      const startPage = Math.max(2, currentPage - pageNeighbours);
+      const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
 
-      const leftBound = currentPage - pageNeighbours;
-      const rightBound = currentPage + pageNeighbours;
-      const beforeLastPage = totalPages - 1;
+      let pages = range(startPage, endPage);
 
-      const startPage = (leftBound > 2) ? leftBound : 2;
-      const endPage = (rightBound < beforeLastPage) ? rightBound : beforeLastPage;
+      /**
+       * hasLeftSpill: has hidden pages to the left
+       * hasRightSpill: has hidden pages to the right
+       * spillOffset: number of hidden pages either to the left or to the right
+       */
+      const hasLeftSpill = startPage > 2;
+      const hasRightSpill = (totalPages - endPage) > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
 
-      pages = range(startPage, endPage);
+      switch (true) {
+        // handle: (1) < {5 6} [7] {8 9} (10)
+        case (hasLeftSpill && !hasRightSpill): {
+          const extraPages = range(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
+        }
 
-      const pagesCount = pages.length;
-      const singleSpillOffset = totalNumbers - pagesCount - 1;
+        // handle: (1) {2 3} [4] {5 6} > (10)
+        case (!hasLeftSpill && hasRightSpill): {
+          const extraPages = range(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
+        }
 
-      const leftSpill = startPage > 2;
-      const rightSpill = endPage < beforeLastPage;
-
-      const leftSpillPage = LEFT_PAGE;
-      const rightSpillPage = RIGHT_PAGE;
-
-      if (leftSpill && !rightSpill) {
-
-        const extraPages = range(startPage - singleSpillOffset, startPage - 1);
-        pages = [leftSpillPage, ...extraPages, ...pages];
-
-      } else if (!leftSpill && rightSpill) {
-
-        const extraPages = range(endPage + 1, endPage + singleSpillOffset);
-        pages = [...pages, ...extraPages, rightSpillPage];
-
-      } else if (leftSpill && rightSpill) {
-        pages = [leftSpillPage, ...pages, rightSpillPage];
+        // handle: (1) < {4 5} [6] {7 8} > (10)
+        case (hasLeftSpill && hasRightSpill):
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
+        }
       }
 
       return [1, ...pages, totalPages];
 
     }
-    
+
     return range(1, totalPages);
 
   }
 
   render() {
 
-    if (!this.totalRecords) return null;
-
-    if (this.totalPages === 1) return null;
+    if (!this.totalRecords || this.totalPages === 1) return null;
 
     const { currentPage } = this.state;
     const pages = this.fetchPageNumbers();
@@ -156,7 +177,7 @@ class Pagination extends Component {
 
               return (
                 <li key={index} className={`page-item${ currentPage === page ? ' active' : ''}`}>
-                  <a className="page-link" href="#" onClick={e => this.handleClick(page, e)}>{ page }</a>
+                  <a className="page-link" href="#" onClick={ this.handleClick(page) }>{ page }</a>
                 </li>
               );
 
@@ -166,6 +187,7 @@ class Pagination extends Component {
         </nav>
       </Fragment>
     );
+
   }
 }
 
